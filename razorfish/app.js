9493216afdeba5c9ec141a685341a604730bafa2
Zachary Fine <zfine416@gmail.com>
2015-06-19 15:35:49 -0400

    debugger

--- razorfish/app.js
+++ razorfish/app.js
@@ -1,11 +1,16 @@
-jQuery(function() {
-	$.ajax({
-	  url: 'gallery_json.js',
-	  dataType: 'json',
-	  success: function(data){
-	  	console.log(data);
-	  	var header = data["album"]["name"];
-	  	console.log(header);
-	  }
-	});
-})
+$.ajax({
+	url: 'gallery_json.js',
+	dataType: 'json',
+	success: function(data){
+		console.log(data);
+		var header = data["album"]["name"];
+		console.log(header);
+		parseData(data);
+	}
+});
+
+function parseData(data){
+	var header = data["album"]["name"];
+	debugger;
+
+}
