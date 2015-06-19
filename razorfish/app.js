b84c82cd46fa974419722daa283d7679cc3c8ad1
Zachary Fine <zfine416@gmail.com>
2015-06-19 16:56:14 -0400

    razor

--- razorfish/app.js
+++ razorfish/app.js
@@ -3,14 +3,19 @@ $.ajax({
 	dataType: 'json',
 	success: function(data){
 		console.log(data);
-		var header = data["album"]["name"];
-		console.log(header);
 		parseData(data);
 	}
 });
 
 function parseData(data){
 	var header = data["album"]["name"];
-	debugger;
-
+	$('.main').append("<p>"+header+"</p>");
+	var photos = data["photos"];
+	var plength = photos.length;
+	for(var i=0; i<plength; i++){
+		$('#car').append("<div><img src="+photos[i]["url"]+"></div>");
+	}
+	$('#car').slick();
 }
+
+
