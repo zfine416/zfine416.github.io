2fe0c8050504c87a93ff41113fc4b8c78ed2f4dd
Zachary Fine <zfine416@gmail.com>
2015-06-30 12:51:35 -0400

    new test site

--- razorfish/app.js
+++ razorfish/app.js
@@ -1,21 +1,12 @@
-$.ajax({
-	url: 'gallery_json.js',
-	dataType: 'json',
-	success: function(data){
-		console.log(data);
-		parseData(data);
-	}
-});
-
-function parseData(data){
-	var header = data["album"]["name"];
-	$('.main').append("<p>"+header+"</p>");
-	var photos = data["photos"];
-	var plength = photos.length;
-	for(var i=0; i<plength; i++){
-		$('#car').append("<div><img src="+photos[i]["url"]+"></div>");
-	}
-	$('#car').slick();
-}
-
-
+jQuery(function() {
+	$.ajax({
+	  url: 'gallery_json.js',
+	  dataType: 'json',
+	  success: function(data){
+	  	console.log(data);
+	  },
+	  error: function(xhr, status, error) {
+	  	console.log(error);
+	  }
+	});
+})
--- test/js/app.js
+++ test/js/app.js
@@ -1,4 +1,6 @@
 console.log("loaded");
+var $me = $('.about .me');
+var $glasses = $('.about .glasses');
 
 
 // var pink = (width) + "px solid hotpink";
@@ -7,7 +9,16 @@ console.log("loaded");
 $( document ).ready(function() {
 	var width = $(window).width();
 	makeTriangle(width);
+	var halfMe = $me.height()/2;
+	centerImage(halfMe);
 });
+
+$(window).scroll(function() {
+	console.log("scrolling");
+	var meWidth = $me.width()*.371;
+	leftGlasses(meWidth);
+});
+
 //function that sets triangle to window width
 function makeTriangle(width){
 	// var width = $(window).width();
@@ -20,4 +31,27 @@ function makeTriangle(width){
 $( window ).resize(function() {
 	var width = $(window).width();
 	makeTriangle(width);
+	var halfMe = $me.height()/2;
+	centerImage(halfMe);
+	var meWidth = $me.width()*.371;
+	leftGlasses(meWidth);
+	console.log(meWidth); 
 });
+
+//center image
+
+function centerImage(halfMe){
+	var half = "calc(50% - " + halfMe + "px)";
+	console.log(this);
+	$me.css('top', half);
+	console.log(half);
+}
+
+//glasses left att equals image width - pixels
+//37.1% of $me.width
+
+function leftGlasses(meWidth){
+	var pix = meWidth + "px";
+	console.log(pix);
+	$glasses.css('left', pix);
+}
