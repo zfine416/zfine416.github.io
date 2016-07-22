40e4d0e8456080492bab94712f1196d955999e60
Zachary Fine <zfine416@gmail.com>
2016-07-22 15:38:37 -0400

    fixed bug

--- app.js
+++ app.js
@@ -13,15 +13,6 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-    setElementWidth('.mini-white-circle');
-
-    $.jInvertScroll(['#new-about'],        // an array containing the selector(s) for the elements you want to animate
-        {
-        height: 4000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
-        onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
-            console.log(percent);
-        }
-    });
 });
 
 var setElementWidth = function(element) {
