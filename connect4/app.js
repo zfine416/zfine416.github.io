735a85738a6e45b892c06e041c01749714ee3af6
Zachary Fine <zfine416@gmail.com>
2017-04-11 16:10:24 -0400

    added react state

--- connect4/app.js
+++ connect4/app.js
@@ -19,10 +19,44 @@
 // There are only 7 * 6 total moves
 'use strict';
 
-
 const board = document.getElementById('board');
-const choices = document.querySelectorAll('.choice');
-let player = 'player1';
+
+const connectFour = {
+  gameState: {
+    currentPlayer: 'player1',
+    moveCount: 0,
+    inProgress: true,
+  },
+  makeMove: function(col) {
+    // get the column number selected
+    const colNum = Number(col.id[col.id.length - 1]);
+    // select the appropriate column elements
+    const cols = document.querySelectorAll(`.column${colNum}`);
+    if(this.gameState.inProgress) {
+      for(let i = cols.length - 1; i >= 0; i--) {
+        if(cols[i].classList[1] !== 'taken') {
+          cols[i].classList.add('taken', this.gameState.currentPlayer);
+          this.gameState.lastMove = cols[i];
+          this.gameState.moveCount++;
+          break;
+        }
+      }
+    }
+    this.gameState.currentPlayer = (this.gameState.currentPlayer === 'player1') ? 'player2' : 'player1';
+  },
+  checkWinner: function() {
+    this.gameState.inProgress = (this.verticalWin() || this.horizontalWin() || this.diagonalWin()) ? false : true;
+  },
+  verticalWin: function() {
+    debugger;
+  },
+  horizontalWin: function() {
+    debugger;
+  },
+  diagonalWin: function() {
+    debugger;
+  }
+}
 
 function game() {
   makeBoard();
@@ -31,7 +65,7 @@ function game() {
 function makeBoard() {
   for(let row = 0; row < 6; row++) {
     const rowEl = document.createElement('div');
-    rowEl.classList.add('row');
+    rowEl.classList.add(`row`);
     board.appendChild(rowEl);
     for(let col = 0; col < 7; col++) {
       const colEl = document.createElement('div');
@@ -41,20 +75,11 @@ function makeBoard() {
   }
 }
 
-choices.forEach((choice) => choice.addEventListener('click', function(){
-  makeMove(this, player);
-  player = (player === 'player1') ? 'player2' : 'player1';
-}));
+const choices = document.querySelectorAll('.choice');
 
-function makeMove(col, player) {
-  const colNum = Number(col.id[col.id.length - 1]);
-  const columns = document.querySelectorAll(`.column${colNum}`);
-  for(let i = columns.length - 1; i >= 0; i--) {
-    if(columns[i].classList[1] !== "taken") {
-      columns[i].classList.add("taken", player);
-      break;
-    }
-  }
-}
+choices.forEach((choice) => choice.addEventListener('click', function(){
+  connectFour.makeMove(this);
+  connectFour.checkWinner();
+}));
 
 game();
