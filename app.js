67f8e52d87d1f538bb1bf467b798801893c91395
Fine <910675@zachary-fine-1682.nyhq.nytint.com>
2017-11-28 17:39:27 -0500

    changed direction of navigation

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ $(window).scroll(function() {
 })
 
 // navigation
-$('.open-overlay').click(function() {
-  var overlay_navigation = $('.overlay-navigation'),
+var overlay_navigation = $('.overlay-navigation'),
     nav_item_1 = $('nav li:nth-of-type(1)'),
     nav_item_2 = $('nav li:nth-of-type(2)'),
     nav_item_3 = $('nav li:nth-of-type(3)'),
@@ -39,6 +38,8 @@ $('.open-overlay').click(function() {
     top_bar = $('.bar-top'),
     middle_bar = $('.bar-middle'),
     bottom_bar = $('.bar-bottom');
+
+$('.open-overlay').click(function() {
 
   overlay_navigation.toggleClass('overlay-active');
   if (overlay_navigation.hasClass('overlay-active')) {
@@ -63,6 +64,19 @@ $('.open-overlay').click(function() {
     nav_item_4.removeClass('slide-in-nav-item-delay-3').addClass('slide-in-nav-item-delay-3-reverse');
     nav_item_5.removeClass('slide-in-nav-item-delay-4').addClass('slide-in-nav-item-delay-4-reverse');
   }
+});
+
+$('nav li').on('click', function() {
+  top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
+    middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
+    bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
+    overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up')
+    nav_item_1.removeClass('slide-in-nav-item').addClass('slide-in-nav-item-reverse');
+    nav_item_2.removeClass('slide-in-nav-item-delay-1').addClass('slide-in-nav-item-delay-1-reverse');
+    nav_item_3.removeClass('slide-in-nav-item-delay-2').addClass('slide-in-nav-item-delay-2-reverse');
+    nav_item_4.removeClass('slide-in-nav-item-delay-3').addClass('slide-in-nav-item-delay-3-reverse');
+    nav_item_5.removeClass('slide-in-nav-item-delay-4').addClass('slide-in-nav-item-delay-4-reverse');
+    overlay_navigation.toggleClass('overlay-active');
 });
 
 // ----------end navigation----------
